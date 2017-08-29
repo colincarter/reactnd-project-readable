9c3e5625797b2e2cc992e67621293fdc9df5773c
Colin Carter <colin_carter@fmail.co.uk>
2017-08-29 20:59:11 +0100

    Adding spectre css

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import configureStore from "./store/configureStore";
 import "./index.css";
 import App from "./Containers/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import "spectre.css/dist/spectre.min.css";
+
 const store = configureStore({});
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route exact path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
