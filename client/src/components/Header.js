86dcaf1a2958e0c3f3c7bda5fc067c045af54afe
Colin Carter <colin_carter@fmail.co.uk>
2017-08-29 21:11:41 +0100

    Adding a prop type to Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = ({ title }) => <h1>{title}</h1>;
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Header;
