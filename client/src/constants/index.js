00b4d0c5e670bb8567db8f39b5950ece4deb0abd
Colin Carter <colin_carter@fmail.co.uk>
2017-09-15 09:35:57 +0100

    Fix for name match on all posts

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -2,5 +2,5 @@ export const ADD_CATEGORIES = "ADD_CATEGORIES";
 export const SET_CATEGORY = "SET_CATEGORY";
 export const ADD_POSTS = "ADD_POSTS";
 export const SET_CURRENT_SORT = "SET_CURRENT_SORT";
-export const ALL_POSTS = "ALL_POSTS";
+export const ALL_POSTS = "all";
 export const ADD_COMMENTS = "ADD_COMMENTS";
