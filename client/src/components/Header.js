5e3bb2b314893cd5ddbf34a40458a9ffc120b164
Colin Carter <colin_carter@fmail.co.uk>
2017-09-17 20:31:15 +0100

    WIP on adding back functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import AppBar from "material-ui/AppBar";
 
-const Header = ({ title }) => <AppBar title={title} />;
+const Header = ({ title, history }) => {
+  const onButtonClick = () => {
+    history.push("/");
+  };
+  return <AppBar title={title} onLeftIconButtonTouchTap={onButtonClick} />;
+};
 
 Header.propTypes = {
   title: PropTypes.string.isRequired
