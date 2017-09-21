97e573350a3a908bbc1dd00e29ac361a1eb0ea07
Colin Carter <colin_carter@fmail.co.uk>
2017-09-21 09:53:04 +0100

    WIP beginning new post form

--- client/src/containers/NewPost.js
+++ client/src/containers/NewPost.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Toolbar, ToolbarGroup, ToolbarTitle } from "material-ui/Toolbar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+
 import Header from "../components/Header";
 
 class NewPost extends React.Component {
+  buttonStyle = {
+    margin: 12
+  };
+
   render() {
     return (
       <MuiThemeProvider>
@@ -15,6 +23,25 @@ class NewPost extends React.Component {
               <ToolbarTitle text="New Post" />
             </ToolbarGroup>
           </Toolbar>
+          <TextField hintText="Post title" />
+          <br />
+          <TextField hintText="Post body" multiLine={true} rows={4} />
+          <br />
+          <TextField hintText="author" />
+          <br />
+          <TextField hintText="category" />
+          <br />
+          <RaisedButton
+            label="Post"
+            primary={true}
+            disabled={true}
+            style={this.buttonStyle}
+          />
+          <RaisedButton
+            label="Reset"
+            secondary={true}
+            style={this.buttonStyle}
+          />
         </div>
       </MuiThemeProvider>
     );
