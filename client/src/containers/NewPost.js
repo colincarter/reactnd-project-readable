0fa163f459d46a247eca33aaf8fbaec45821eb6d
Colin Carter <colin_carter@fmail.co.uk>
2017-09-24 07:44:32 +0100

    Provide components for a New Post

--- client/src/containers/NewPost.js
+++ client/src/containers/NewPost.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import uniqid from "uniqid";
 
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Toolbar, ToolbarGroup, ToolbarTitle } from "material-ui/Toolbar";
@@ -8,12 +11,46 @@ import RaisedButton from "material-ui/RaisedButton";
 
 import Header from "../components/Header";
 
+import * as actionCreators from "../actions";
+
 class NewPost extends React.Component {
+  state = {
+    title: "",
+    body: "",
+    author: "",
+    category: ""
+  };
+
   buttonStyle = {
     margin: 12
   };
 
+  handleOnChange = event => {
+    const name = event.target.name;
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleReset = () => {
+    this.setState({
+      title: "",
+      body: "",
+      author: "",
+      category: ""
+    });
+  };
+
+  handlePost = () => {
+    const newPost = { id: uniqid(), timestamp: Date.now(), ...this.state };
+    this.props.createPost(newPost);
+  };
+
   render() {
+    const canPost =
+      this.state.title &&
+      this.state.body &&
+      this.state.author &&
+      this.state.category;
+
     return (
       <MuiThemeProvider>
         <div>
@@ -23,24 +60,48 @@ class NewPost extends React.Component {
               <ToolbarTitle text="New Post" />
             </ToolbarGroup>
           </Toolbar>
-          <TextField hintText="Post title" />
+          <TextField
+            hintText="Post title"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleOnChange}
+          />
           <br />
-          <TextField hintText="Post body" multiLine={true} rows={4} />
+          <TextField
+            hintText="Post body"
+            name="body"
+            multiLine={true}
+            rows={4}
+            value={this.state.body}
+            onChange={this.handleOnChange}
+          />
           <br />
-          <TextField hintText="author" />
+          <TextField
+            hintText="author"
+            name="author"
+            value={this.state.author}
+            onChange={this.handleOnChange}
+          />
           <br />
-          <TextField hintText="category" />
+          <TextField
+            hintText="category"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleOnChange}
+          />
           <br />
           <RaisedButton
             label="Post"
             primary={true}
-            disabled={true}
+            disabled={!canPost}
             style={this.buttonStyle}
+            onClick={this.handlePost}
           />
           <RaisedButton
             label="Reset"
             secondary={true}
             style={this.buttonStyle}
+            onClick={this.handleReset}
           />
         </div>
       </MuiThemeProvider>
@@ -49,7 +110,11 @@ class NewPost extends React.Component {
 }
 
 NewPost.propTypes = {
-  history: PropTypes.object.isRequired
+  history: PropTypes.object.isRequired,
+  createPost: PropTypes.func.isRequired
 };
 
-export default NewPost;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+export default connect(null, mapDispatchToProps)(NewPost);
