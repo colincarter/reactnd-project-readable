08894c2f2e2c020a5de543b1e36fb0c3b21ca20b
Colin Carter <colin_carter@fmail.co.uk>
2017-09-26 09:55:47 +0100

    Initial work on deleting a post

--- client/src/containers/PostSummaries.js
+++ client/src/containers/PostSummaries.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import { List, ListItem } from "material-ui/List";
 import { Toolbar, ToolbarGroup, ToolbarTitle } from "material-ui/Toolbar";
@@ -11,7 +11,8 @@ import MenuItem from "material-ui/MenuItem";
 import IconMenu from "material-ui/IconMenu";
 import IconButton from "material-ui/IconButton";
 import FontIcon from "material-ui/FontIcon";
-
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
+import { grey400 } from "material-ui/styles/colors";
 import * as actionCreators from "../actions";
 import { ALL_POSTS } from "../constants";
 import formattedTimestamp from "../lib/formattedTimestamp";
@@ -23,12 +24,37 @@ class PostSummaries extends React.Component {
     textDecoration: "none"
   };
 
+  renderIconMenu = postId => {
+    const iconButtonElement = (
+      <IconButton touch={true} tooltip="more" tooltipPosition="bottom-left">
+        <MoreVertIcon color={grey400} />
+      </IconButton>
+    );
+    return (
+      <IconMenu iconButtonElement={iconButtonElement}>
+        <MenuItem>Edit</MenuItem>
+        <MenuItem onClick={() => this.handleDeletePost(postId)}>
+          Delete
+        </MenuItem>
+      </IconMenu>
+    );
+  };
+
   componentDidMount = () => {
     this.props.setCurrentSort("voteScore");
   };
 
   onSortChange = (event, value) => {
     this.props.setCurrentSort(value);
+  };
+
+  handleListClick = postId => {
+    const url = `/post/${postId}/comments`;
+    this.props.history.push(url);
+  };
+
+  handleDeletePost = postId => {
+    console.log(postId);
   };
 
   render = () => {
@@ -56,26 +82,22 @@ class PostSummaries extends React.Component {
         </Toolbar>
         <List>
           {this.props.postsForCategory.map((post, i) => (
-            <Link
-              to={`/post/${post.id}/comments`}
-              style={this.linkStyle}
+            <ListItem
               key={i}
-            >
-              <ListItem
-                key={i}
-                primaryText={
-                  <p>
-                    {post.voteScore} <span>{post.title}</span>
-                  </p>
-                }
-                secondaryText={
-                  <p>
-                    {post.category}{" "}
-                    <span>{formattedTimestamp(post.timestamp)}</span>
-                  </p>
-                }
-              />
-            </Link>
+              primaryText={
+                <p>
+                  {post.voteScore} <span>{post.title}</span>
+                </p>
+              }
+              secondaryText={
+                <p>
+                  {post.category}{" "}
+                  <span>{formattedTimestamp(post.timestamp)}</span>
+                </p>
+              }
+              rightIconButton={this.renderIconMenu(post.id)}
+              onClick={() => this.handleListClick(post.id)}
+            />
           ))}
         </List>
       </div>
@@ -84,7 +106,8 @@ class PostSummaries extends React.Component {
 }
 
 PostSummaries.propTypes = {
-  postsForCategory: PropTypes.arrayOf(Object).isRequired
+  postsForCategory: PropTypes.arrayOf(Object).isRequired,
+  history: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, props) {
@@ -104,4 +127,6 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostSummaries);
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(PostSummaries)
+);
